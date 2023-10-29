f4c09d088deb94f457d1ecc179e13319a5130aa4
chase <yun.er.run@gmail.com>
2023-10-29 23:03:29 +0900

    Use reply for chat answers

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use serde_json::json;
-use tg_flows::{listen_to_update, Telegram, Update, UpdateKind, update_handler};
+use flowsnet_platform_sdk::logger;
 use openai_flows::{
     chat::{ChatModel, ChatOptions},
     OpenAIFlows,
 };
+use serde_json::json;
 use store_flows::{get, set};
-use flowsnet_platform_sdk::logger;
+use tg_flows::{listen_to_update, update_handler, Message, Telegram, Update, UpdateKind};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -14,13 +14,42 @@ pub async fn on_deploy() {
     listen_to_update(telegram_token).await;
 }
 
+trait TelegramExtension {
+    fn reply_to_message<T>(&self, msg: &Message, text: T) -> anyhow::Result<Message>
+    where
+        T: Into<String>;
+}
+
+impl TelegramExtension for Telegram {
+    fn reply_to_message<T>(&self, msg: &Message, text: T) -> anyhow::Result<Message>
+    where
+        T: Into<String>,
+    {
+        let text: String = text.into();
+        let body = serde_json::json!({
+            "chat_id": msg.chat.id,
+            "reply_to_message_id": msg.id,
+            "text": text,
+        });
+        self.request(tg_flows::Method::SendMessage, body.to_string().as_bytes())
+    }
+}
+
 #[update_handler]
 async fn handler(update: Update) {
     logger::init();
     let telegram_token = std::env::var("telegram_token").unwrap();
     let placeholder_text = std::env::var("placeholder").unwrap_or("Typing ...".to_string());
-    let system_prompt = std::env::var("system_prompt").unwrap_or("You are a helpful assistant answering questions on Telegram.".to_string());
-    let help_mesg = std::env::var("help_mesg").unwrap_or("I am your assistant on Telegram. Ask me any question! To start a new conversation, type the /restart command.".to_string());
+    let system_prompt = std::env::var("system_prompt")
+        .unwrap_or("You are a helpful assistant answering questions on Telegram.".to_string());
+    let help_mesg = std::env::var("help_mesg").unwrap_or(
+        r#"
+        I am your assistant on Telegram. Ask me any question!
+        To start a new conversation, type the /restart command.
+        To print this message, type /help.
+        "#
+        .to_string(),
+    );
 
     let tele = Telegram::new(telegram_token.to_string());
 
@@ -39,20 +68,17 @@ async fn handler(update: Update) {
         let text = msg.text().unwrap_or("");
         if text.eq_ignore_ascii_case("/help") {
             _ = tele.send_message(chat_id, &help_mesg);
-
         } else if text.eq_ignore_ascii_case("/start") {
             _ = tele.send_message(chat_id, &help_mesg);
             set(&chat_id.to_string(), json!(true), None);
             log::info!("Started converstion for {}", chat_id);
-
         } else if text.eq_ignore_ascii_case("/restart") {
             _ = tele.send_message(chat_id, "Ok, I am starting a new conversation.");
             set(&chat_id.to_string(), json!(true), None);
             log::info!("Restarted converstion for {}", chat_id);
-
         } else {
             let placeholder = tele
-                .send_message(chat_id, &placeholder_text)
+                .reply_to_message(&msg, &placeholder_text)
                 .expect("Error occurs when sending Message to Telegram");
 
             let restart = match get(&chat_id.to_string()) {
@@ -65,12 +91,19 @@ async fn handler(update: Update) {
                 co.restart = true;
             }
 
-            match openai.chat_completion(&chat_id.to_string(), &text, &co).await {
+            match openai
+                .chat_completion(&chat_id.to_string(), &text, &co)
+                .await
+            {
                 Ok(r) => {
                     _ = tele.edit_message_text(chat_id, placeholder.id, r.choice);
                 }
                 Err(e) => {
-                    _ = tele.edit_message_text(chat_id, placeholder.id, "Sorry, an error has occured. Please try again later!");
+                    _ = tele.edit_message_text(
+                        chat_id,
+                        placeholder.id,
+                        "Sorry, an error has occured. Please try again later!",
+                    );
                     log::error!("OpenAI returns error: {}", e);
                 }
             }
